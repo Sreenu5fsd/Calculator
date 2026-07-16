1f1fa69c1e96b89fd581bd1ec14e86c0da0d1b23
Sreenu J <sreenujogi123@gmail.com>
2026-07-16 11:16:44 +0530

    Subtraction function added in Calculator

--- Calculator.java
+++ Calculator.java
@@ -13,4 +13,7 @@ public class Calculator{
     public static int addition(int a , int b){
         return a  + b;
     }
+    public static int subtraction(int a , int b){
+        return a - b;
+    }
 }
